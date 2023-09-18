f02178bc9651ad187694c6b43204a6659fe64b86
esmecelatocorretor <143470274+esmecelatocorretor@users.noreply.github.com>
2023-09-18 10:05:17 -0300

    Update enviar_emails.py
    yahoo

--- enviar_emails.py
+++ enviar_emails.py
@@ -4,10 +4,12 @@ import random
 import time
 
 # Configuração SMTP para o Hotmail/Outlookk
-smtp_server = 'smtp.live.com'  # Ou 'smtp-mail.outlook.com'
+# Configuração SMTP para o Yahoo
+smtp_server = 'smtp.mail.yahoo.com'
 smtp_port = 587
-username = 'esmecelato@hotmail.com'
-password = 'kPEn6xFZwfKCEE4'
+username = 'esmecelato@yahoo.com.br'
+password = 'caboedson7'
+
 
 # Ler a lista de e-mails do arquivo CSV
 with open('lista_emails.csv', 'r') as file:
@@ -18,11 +20,11 @@ while True:
     # Escolher um e-mail aleatório da lista
     random_email = random.choice(email_list)
 
-    try:
-        # Configurar a conexão SMTP
-        with smtplib.SMTP(smtp_server, smtp_port) as server:
-            server.starttls()
-            server.login(username, password)
+   # Configurar a conexão SMTP
+try:
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(username, password)
 
             # Preencher o corpo do e-mail
             subject = 'Apresentação - Corretor de Imóveis Edson Esmecelato'
@@ -40,7 +42,7 @@ while True:
 
         print(f'E-mail enviado para {random_email}')
     except smtplib.SMTPException as e:
-        print(f"Erro ao enviar o e-mail: {str(e)}")
+    print(f"Erro ao enviar o e-mail: {str(e)}")
 
     # Esperar 5 minutos antes de enviar o próximo e-mail (300 segundos)
     time.sleep(60)
